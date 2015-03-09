68c59ee629200547b596dbc14440644889b3760c
Tobias Nyholm <tobias.nyholm@gmail.com>
2015-03-09 18:35:51 +0100

    Fixed indentations

--- src/Dmishh/Bundle/SettingsBundle/Manager/SettingsManagerInterface.php
+++ src/Dmishh/Bundle/SettingsBundle/Manager/SettingsManagerInterface.php
@@ -20,52 +20,52 @@ interface SettingsManagerInterface
     const SCOPE_USER   = 'user';
 
     /**
-     * Returns setting value by its name
+     * Returns setting value by its name.
      *
-     * @param string $name
+     * @param string            $name
      * @param SettingOwner|null $owner
      *
-*@return mixed
+     * @return mixed
      */
-    function get($name, SettingOwner $owner = null);
+    public function get($name, SettingOwner $owner = null);
 
     /**
-     * Returns all settings as associative name-value array
+     * Returns all settings as associative name-value array.
      *
      * @param SettingOwner|null $owner
      *
-*@return array
+     * @return array
      */
-    function all(SettingOwner $owner = null);
+    public function all(SettingOwner $owner = null);
 
     /**
-     * Sets setting value by its name
+     * Sets setting value by its name.
      *
-     * @param string $name
-     * @param mixed $value
+     * @param string            $name
+     * @param mixed             $value
      * @param SettingOwner|null $owner
      *
-*@return SettingsManagerInterface
+     * @return SettingsManagerInterface
      */
-    function set($name, $value, SettingOwner $owner = null);
+    public function set($name, $value, SettingOwner $owner = null);
 
     /**
-     * Sets settings' values from associative name-value array
+     * Sets settings' values from associative name-value array.
      *
-     * @param array $settings
+     * @param array             $settings
      * @param SettingOwner|null $owner
      *
-*@return SettingsManagerInterface
+     * @return SettingsManagerInterface
      */
-    function setMany(array $settings, SettingOwner $owner = null);
+    public function setMany(array $settings, SettingOwner $owner = null);
 
     /**
-     * Clears setting value
+     * Clears setting value.
      *
-     * @param string $name
+     * @param string            $name
      * @param SettingOwner|null $owner
      *
-*@return SettingsManagerInterface
+     * @return SettingsManagerInterface
      */
-    function clear($name, SettingOwner $owner = null);
+    public function clear($name, SettingOwner $owner = null);
 }
