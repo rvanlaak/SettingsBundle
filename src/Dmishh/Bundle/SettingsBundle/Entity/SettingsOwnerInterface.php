355f3ebe5a3ee9b264a1eeb3d7760050d58d994e
dmishh <dmishh@gmail.com>
2015-03-30 23:17:16 +0300

    Updated composer and added default comments header to the SettingsOwnerInterface

--- src/Dmishh/Bundle/SettingsBundle/Entity/SettingsOwnerInterface.php
+++ src/Dmishh/Bundle/SettingsBundle/Entity/SettingsOwnerInterface.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This file is part of the DmishhSettingsBundle package.
+ *
+ * (c) 2013 Dmitriy Scherbina <http://dmishh.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Dmishh\Bundle\SettingsBundle\Entity;
 
 /**
@@ -8,4 +17,4 @@ namespace Dmishh\Bundle\SettingsBundle\Entity;
 interface SettingsOwnerInterface
 {
     public function getSettingIdentifier();
-} 
+}
