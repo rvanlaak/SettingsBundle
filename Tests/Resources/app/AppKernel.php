1dafb44ff7ed9a344833f5380dbcce3abed81d56
Nicolas PHILIPPE <nikophil@gmail.com>
2018-03-05 16:10:09 +0100

    fix travis error with kernel class

--- Tests/Resources/app/AppKernel.php
+++ Tests/Resources/app/AppKernel.php
@@ -24,7 +24,7 @@ class AppKernel extends Kernel
      */
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load(__DIR__.'/config/config.yml');
+        $loader->load(__DIR__ . '/config/config.yml');
     }
 
     /**
