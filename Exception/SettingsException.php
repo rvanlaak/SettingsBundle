4426319ef91eb050f93e37097c1e1c0a954b8ca9
Richard van Laak <rvanlaak@gmail.com>
2019-12-13 09:53:58 +0100

    Update outdated license years

--- Exception/SettingsException.php
+++ Exception/SettingsException.php
@@ -1,14 +1,5 @@
 <?php
 
-/**
- * This file is part of the DmishhSettingsBundle package.
- *
- * (c) 2013 Dmitriy Scherbina <http://dmishh.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Dmishh\SettingsBundle\Exception;
 
 interface SettingsException extends \Throwable
